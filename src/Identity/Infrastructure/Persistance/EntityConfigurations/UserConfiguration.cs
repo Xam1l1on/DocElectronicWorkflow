fa9a9df2d0c14254e2623c81bf2600f8d9fc1e13
Xam1l1on <kir.solomatin@gmail.com>
2026-06-09 21:23:46 +0300

    Refactor UserConfiguration to enhance index and relationship configurations

--- src/Identity/Infrastructure/Persistance/EntityConfigurations/UserConfiguration.cs
+++ src/Identity/Infrastructure/Persistance/EntityConfigurations/UserConfiguration.cs
@@ -1,5 +1,6 @@
 using Identity.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 namespace Identity.Infrastructure.Persistance.EntityConfigurations
 {
@@ -7,7 +8,18 @@ namespace Identity.Infrastructure.Persistance.EntityConfigurations
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
+            builder.HasIndex(x => new { x.UserRoles, x.Id }).IsUnique();
             builder.HasIndex(x => x.Id).IsUnique();
+            builder.HasKey(x => x.Id);
+            builder.HasIndex(x=> new { x.UserName, x.Id }).IsUnique();
+            builder.HasMany(x => x.UserRoles)
+                .WithOne(x => x.User)
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+             builder.HasMany(x => x.UserClaims)
+                .WithOne(x => x.User)
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
